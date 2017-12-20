3e10286fc2e6735141f8028ff1c52dc732b487ce
mdunnettstone <mdunnettstone@gmail.com>
2017-12-20 15:20:51 +0000

    Start lister pages (index and category) - tweak product page

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,12 +7,12 @@ class Product < ApplicationRecord
   accepts_nested_attributes_for :product_images
 
   def average_review
-    average = reviews.average('rating')
-    score   = average.round(1)
-    count   = reviews.count
-    if reviews.nil?
+    if reviews.blank?
       return "No reviews yet, be the first to add one!"
     else
+      average = reviews.average('rating')
+      score   = average.round(1)
+      count   = reviews.count
       "#{score}/5.0 (#{count})"
     end
   end
