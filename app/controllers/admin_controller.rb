89755427549e91ba1d75448c74625b2e5b781a00
mdunnettstone <mdunnettstone@gmail.com>
2018-03-12 12:57:43 +0000

    Add hero carousel. Saving before trying different OC2 gem

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,7 @@ class AdminController < ApplicationController
   before_action :require_admin
 
   def require_admin
-    unless current_user.admin
+    unless user_signed_in? && current_user.admin
       render plain: 'Unauthorized', status: :unauthorized
     end
   end
