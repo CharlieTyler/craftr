92bcb09bf1ec0e0bd9c514802e18cc9ca5e03022
mdunnettstone <mdunnettstone@gmail.com>
2018-03-13 17:48:58 +0000

    Add admin dashboard link to footer for admins

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     @featured_categories = Category.where(featured: true).first(3)
     @recipes             = Recipe.last(3)
-    @featured_products   = Product.where(featured: true).first(4)
+    @featured_products   = Product.all
     @featured_product    = Product.find(2)
     # @instas              = InstagramApi.user.recent_media
   end
