cd38f5e69acd7588501c65a077be2343aea53f5f
mdunnettstone <mdunnettstone@gmail.com>
2018-03-15 17:30:28 +0000

    Add other products from same distillery

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,10 @@
 class ProductsController < ApplicationController
   def show
-    @product          = Product.friendly.find(params[:id])
-    @distillery       = @product.distillery
-    @review           = Review.new
-    @recipes          = @product.recipes
+    @product                   = Product.friendly.find(params[:id])
+    @distillery                = @product.distillery
+    @review                    = Review.new
+    @recipes                   = @product.recipes
+    @other_distillery_products = @product.distillery.products.where.not(id: @product.id)
     if user_signed_in?
       current_user.viewed_products << @product
     end
