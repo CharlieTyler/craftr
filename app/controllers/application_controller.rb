37357c6fb6dd3a722dcaffc69db2009b55261b26
mdunnettstone <mdunnettstone@gmail.com>
2018-03-16 17:35:08 +0000

    Deal with signing in and signing out before and after creating cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,25 +4,22 @@ class ApplicationController < ActionController::Base
 
   private
 
-  # Copied from https://jedrekdomanski.wordpress.com/2017/02/05/building-a-shopping-cart-in-ruby-on-rails-part-1/
-  # def set_cart
-  #   @cart = Cart.find(session[:cart_id])
-  # rescue ActiveRecord::RecordNotFound
-  #   @cart = Cart.create
-  #   session[:cart_id] = @cart.id
-  # end
-
-
-  # currently doesn't account for users - probably want something like this... to be checked with Matt
-
+  # Copied from https://jedrekdomanski.wordpress.com/2017/02/05/building-a-shopping-cart-in-ruby-on-rails-part-1/ and then altered to deal with Devise
+  # currently does not assign session cart to user if subsequently logs in
   def set_cart
     if user_signed_in?
       if current_user.cart.present? #based on a has_one relationship - note: never used this yet
         @cart = current_user.cart
+      elsif session[:cart_id].present? # if built a cart without being logged in and is now logged in
+        @cart = Cart.find(session[:cart_id])
+        if @cart.user_id.blank? # if hasn't already updated
+          @cart.update_attributes(user_id: current_user.id) # update user
+        end
+        session[:cart_id] = nil # and remove from unassigned carts
       else
         @cart = Cart.create(user_id: current_user.id)
       end
-    elsif Cart.find(session[:cart_id]).exists?
+    elsif session[:cart_id].present?
       @cart = Cart.find(session[:cart_id])
     else
       @cart = Cart.create
--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -2,6 +2,10 @@ class CartProduct < ApplicationRecord
   belongs_to :cart
   belongs_to :product
 
+  validates :product, presence: true
+  validates :cart, presence: true
+  validates :quantity, numericality: { only_integer: true, greater_than: 0, less_than: 20 }
+
   def subtotal
     (quantity * product.price).to_f / 100
   end
