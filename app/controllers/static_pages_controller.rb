aa8eaa57dbb06e2196f89c379563fca12c15a8bc
mdunnettstone <mdunnettstone@gmail.com>
2018-03-31 16:32:25 +0000

    See all links on homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
-    @featured_categories   = Category.where(featured: true).first(3)
+    @featured_categories   = Category.where(featured: true).first(4)
     @recipes               = Recipe.last(3)
-    @featured_products     = Product.all
+    @featured_products     = Product.where(featured: true).first(4)
     @featured_product      = Product.find(2)
-    @featured_distilleries = Distillery.last(3)
+    @featured_distilleries = Distillery.last(4)
     @instas              = InstagramApi.user.recent_media
   end
 
