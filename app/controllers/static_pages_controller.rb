7160a3887551760cfd33ad851c7b2faa75303c7d
mdunnettstone <mdunnettstone@gmail.com>
2018-04-01 18:32:37 +0000

    Minor tweaks to homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,6 @@ class StaticPagesController < ApplicationController
     @featured_categories   = Category.where(featured: true).first(4)
     @recipes               = Recipe.last(3)
     @featured_products     = Product.where(featured: true).first(4)
-    @featured_product      = Product.find(2)
     @featured_distilleries = Distillery.last(4)
     @instas              = InstagramApi.user.recent_media
   end
