65424520dcc7c9e4a5496cb9f3e5b89af0f269c8
mdunnettstone <mdunnettstone@gmail.com>
2018-04-02 18:25:25 +0000

    Leverage featured products, recipes and articles on category show page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,6 +5,9 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.friendly.find(params[:id])
+    @featured_products = @category.products.where(featured: true)
+    @featured_recipes = @category.recipes.where(featured: true)
+    @featured_articles = @category.articles.where(featured: true)
     @products = @category.products
     @recipes  = @category.recipes
     @articles = @category.articles
