b4f15298bb47da42e54787df8f781680eb9a8839
mdunnettstone <mdunnettstone@gmail.com>
2018-04-06 00:57:41 +0000

    Add privacy policy

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get "/about" => "static_pages#about"
   get "/contact" => "static_pages#contact"
+  get "/privacy-policy" => "static_pages#privacy"
 
   get "/search" => "search#search" 
 
