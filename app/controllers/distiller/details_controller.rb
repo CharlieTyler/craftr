4281dfd8ecdbfe3a9bf152cee3dab606934fc140
mdunnettstone <mdunnettstone@gmail.com>
2018-04-28 20:19:57 +0000

    Process Stripe Connect account additions, store in database

--- app/controllers/distiller/details_controller.rb
+++ app/controllers/distiller/details_controller.rb
@@ -8,14 +8,17 @@ class Distiller::DetailsController < DistillersController
   end
 
   def register_stripe_key
-    if current_distillery.update_attributes({
-          stripe_id: request.env["omniauth.auth"].credentials.token
-        })
-      flash[:notice] = "Successfully connected to Stripe"
-      redirect_to distiller_transactional_checklist_path
-    else
-      flash[:alert] = "Error connecting to Stripe"
-      redirect_to distiller_transactional_checklist_path
-    end
+    options = {
+      site: 'https://connect.stripe.com',
+      authorize_url: '/oauth/authorize',
+      token_url: '/oauth/token'
+    }
+    code = params[:code]
+    client = OAuth2::client.new(ENV['CLIENT_ID'], ENV['SECRET_KEY'], options)
+    @resp = client.auth_code.get_token(code, :params => {:scope => 'read_write'})
+    @access_token = @resp.token
+    current_distillery.update_attributes(stripe_id: @resp.params["stripe_user_id"]) if @resp
+    flash[:notice] = "Your account has been successfully created and is ready to process payments!"
+    redirect_to distillery_dashboard_path
   end
 end
