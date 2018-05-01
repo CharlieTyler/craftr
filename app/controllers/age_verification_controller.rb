9a4baa0c1da0ac655645124fe2ba0bd7d5a9bccf
mdunnettstone <mdunnettstone@gmail.com>
2018-05-01 13:34:04 +0000

    Make age verification respond to js

--- app/controllers/age_verification_controller.rb
+++ app/controllers/age_verification_controller.rb
@@ -1,7 +1,8 @@
 class AgeVerificationController < ApplicationController
+  respond_to :js
+
   def create
     # Should only get here if user not logged in
     session[:age_verified] = true 
-    redirect_to request.referrer
   end
 end
