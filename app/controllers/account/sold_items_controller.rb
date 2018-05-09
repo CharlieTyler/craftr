d7f04a42cdcd711d9c7eba2b67e9fb8c37bc27e1
mdunnettstone <mdunnettstone@gmail.com>
2018-05-09 21:36:33 +0000

    Fix sold_items controller typo

--- app/controllers/account/sold_items_controller.rb
+++ app/controllers/account/sold_items_controller.rb
@@ -1,9 +1,9 @@
 class Account::SoldItemsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def show
     @sold_item = SoldItem.find(params[:id])
-    unless @sold_item.user == current user
+    unless @sold_item.user == current_user
       flash[:alert] = "You may only view your own order"
       redirect_to me_path
     end
