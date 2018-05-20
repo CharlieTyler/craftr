f49f1036c4bc30a1f9e7c8aa99f3ec8926c5baaa
mdunnettstone <mdunnettstone@gmail.com>
2018-05-20 16:11:19 +0000

    Revamp homepage to remove categories

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
     @carousel_features     = CarouselFeature.rank(:row_order).all
+    @all_categories        = Category.all
     @featured_categories   = Category.where(featured: true).first(4)
     @recipes               = Recipe.last(3)
-    @featured_products     = Product.where(featured: true).first(6)
     @featured_distilleries = Distillery.last(4)
     # @instas              = InstagramApi.user.recent_media
   end
