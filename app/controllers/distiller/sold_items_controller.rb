5522decfe372c7bed905095256d329fb5da0f55f
mdunnettstone <mdunnettstone@gmail.com>
2018-06-06 22:45:33 +0000

    Add shipped orders index

--- app/controllers/distiller/sold_items_controller.rb
+++ app/controllers/distiller/sold_items_controller.rb
@@ -1,9 +1,13 @@
 class Distiller::SoldItemsController < DistillersController
-  before_action :find_sold_item
-  before_action :verify_order_item_belongs_to_distiller
+  before_action :find_sold_item, only: [:show]
+  before_action :verify_order_item_belongs_to_distiller, only: [:show]
 
   def show
     @postages = @sold_item.postages.order("created_at DESC")
+  end
+
+  def index
+    @fulfilled_sold_items = current_distillery.sold_items.where(shipped: true)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -75,7 +75,7 @@ Rails.application.routes.draw do
     get "/dashboard", :controller => "static_pages", :action => "dashboard"
     get "/transactional_checklist" => "details#transactional_checklist"
     resources :addresses, only: [:new, :create, :edit, :update]
-    resources :sold_items, only: [:show]
+    resources :sold_items, only: [:show, :index]
     get "/register-stripe-key", :controller => "details", :action => "register_stripe_key"
     resources :products, only: [:edit, :update] do
       member do
