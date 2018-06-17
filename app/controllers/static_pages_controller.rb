290f2690fa487e4e4f64519c55989a95b78bb099
mdunnettstone <mdunnettstone@gmail.com>
2018-06-17 22:00:43 +0000

    Add page description to homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,7 @@ class StaticPagesController < ApplicationController
     @featured_categories   = Category.where(featured: true).first(4)
     @recipes               = Recipe.last(3)
     @featured_distilleries = Distillery.last(4)
+    @page_description      = "The UK's only marketplace and community dedicated to local, independent craft spirits. Discover new gins, vodkas and other spirits, and buy directly from the distillers."
     # @instas              = InstagramApi.user.recent_media
   end
 
