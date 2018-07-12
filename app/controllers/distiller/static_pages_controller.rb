bae7c47111bd333c31ec4e8fb3689b11e480b24b
mdunnettstone <mdunnettstone@gmail.com>
2018-07-12 20:10:09 +0000

    Add some validaAdd some untested validations to batches, and allow for sold_items without postage labels yet in dashboard

--- app/controllers/distiller/static_pages_controller.rb
+++ app/controllers/distiller/static_pages_controller.rb
@@ -1,9 +1,10 @@
 class Distiller::StaticPagesController < DistillersController
   def dashboard
-    @products               = current_distillery.products
-    @unbatched_sold_items = current_distillery.sold_items.where(batch_id: nil).order("created_at DESC")
-    @unshipped_batches      = current_distillery.batches.where(shipped: false).order("created_at DESC")
-    @batch                  = Batch.new
+    @products                                 = current_distillery.products
+    @unshipped_batches                        = current_distillery.batches.where(shipped: false).order("created_at DESC")
+    @unbatched_sold_items_with_postage_labels = current_distillery.sold_items.where(batch_id: nil).order("created_at DESC")
+    @sold_items_without_postage_labels        = current_distillery.sold_items.where(batch_id: nil, shipping_label_created: false).order("created_at DESC")
+    @batch                                    = Batch.new
     @batch.sold_items.build
   end
 
--- app/models/batch.rb
+++ app/models/batch.rb
@@ -2,9 +2,11 @@ class Batch < ApplicationRecord
   include ActionView::Helpers::TextHelper
   has_many :sold_items
   has_many :postages, through: :sold_items
-
   belongs_to :distillery
+
   accepts_nested_attributes_for :sold_items, reject_if: proc { |attributes| attributes['sold_item_id'].blank? }
+
+  validate :sold_items_have_shipping_labels_and_belong_to_distiller
 
   def description
     description = "#{pluralize(sold_items.sum(&:quantity), 'parcel')}, batched at #{created_at.strftime('%B %d, %Y at %H:%M')}"  
@@ -21,6 +23,14 @@ class Batch < ApplicationRecord
       return "Awaiting shipping"
     else
       return "Awaiting shipping label"
+    end
+  end
+
+  def sold_items_have_shipping_labels_and_belong_to_distiller
+    sold_items.each do |si|
+      unless si.shipping_label_created && si.distillery == distillery
+        return false
+      end
     end
   end
 end
