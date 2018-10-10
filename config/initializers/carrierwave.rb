45a8ce1ece75c5e74e94a473238f2040a829c8d8
mdunnettstone <mdunnettstone@gmail.com>
2018-10-10 03:37:00 +0000

    Fix Carrierwave ENV variables

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,5 +5,5 @@ CarrierWave.configure do |config|
     aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],        # required
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],        # required
   }
-  config.fog_directory  = ENV["AWS_FOG_DIRECTORY"]              # required
+  config.fog_directory  = ENV["FOG_DIRECTORY"]              # required
 end
