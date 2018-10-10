91fb9a063b70e44d33f5fa249248e6ad37b75bcd
mdunnettstone <mdunnettstone@gmail.com>
2018-10-10 02:57:08 +0000

    Attempt 1 at allowing write on heroku sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,4 +1,21 @@
-SitemapGenerator::Sitemap.default_host = "https://www.craftr.co.uk"
+require 'fog-aws'
+
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(fog_provider: 'AWS',
+                                         aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+                                         aws_secret_access_key: ENV['AWS_SECRET_KEY'],
+                                         fog_directory: ENV['AWS_BUCKET'],
+                                         fog_region: ENV['AWS_REGION'])
+
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "http://www.craftr.co.uk"
+# pick a place safe to write the files
+SitemapGenerator::Sitemap.public_path = 'tmp/'
+# store on S3 using Fog (pass in configuration values as shown above if needed)
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new
+# inform the map cross-linking where to find the other maps
+# SitemapGenerator::Sitemap.sitemaps_host = "http://#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com/"
+# pick a namespace within your bucket to organize your maps
+SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 
 SitemapGenerator::Sitemap.create do
   # Static pages
