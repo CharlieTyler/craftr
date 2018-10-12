ea6abd87f75148905757c6cf27c659189bb52ae3
mdunnettstone <mdunnettstone@gmail.com>
2018-10-12 04:48:10 +0000

    Fix top banner and product partial button on mobile + graphing

--- app/controllers/admin/static_pages_controller.rb
+++ app/controllers/admin/static_pages_controller.rb
@@ -4,6 +4,7 @@ class Admin::StaticPagesController < AdminController
     @unbatched_sold_items_with_postage_label = SoldItem.where(batch_id: nil, shipping_label_created: true).order("created_at DESC")
     @unbatched_sold_items_without_postage_label = SoldItem.where(batch_id: nil, shipping_label_created: false).order("created_at DESC")
     @transactional_distilleries = Distillery.where.not(stripe_id: nil)
+    @last_30_days_sales = SoldItem.where("(created_at > ?)", Time.now.in_time_zone('London') - 30.days)
   end
 
   def reports
