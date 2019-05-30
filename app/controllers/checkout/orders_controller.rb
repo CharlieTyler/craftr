d262a1fd9356286f104a5b64670a5548c2f8fbd5
mdunnettstone <mdunnettstone@gmail.com>
2019-05-30 10:18:02 +0000

    Remove beta tester requirement

--- app/controllers/checkout/orders_controller.rb
+++ app/controllers/checkout/orders_controller.rb
@@ -4,7 +4,7 @@ class Checkout::OrdersController < ApplicationController
   before_action :check_all_products_and_distilleries_transactional, except: [:confirmation]
   before_action :check_order_has_shipping_type, except: [:update_shipping, :confirmation]
   before_action :check_order_has_address, only: [:payment, :charge_payment]
-  before_action :check_user_is_beta_tester, only: [:payment, :charge_payment]
+  # before_action :check_user_is_beta_tester, only: [:payment, :charge_payment]
 
   def update_shipping
     @order.update_attributes(order_shipping_params)
