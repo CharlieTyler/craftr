209a98108c58ef2dc3201a440ba3c3d1d1bd5ddf
mdunnettstone <mdunnettstone@gmail.com>
2019-06-14 10:42:37 +0000

    Move add new address out of modal

--- app/assets/javascripts/everywhere.js
+++ app/assets/javascripts/everywhere.js
@@ -72,43 +72,26 @@ $(document).on('turbolinks:load', function() {
 
 });
 
-$(document).on('turbolinks:load', function() { 
-  var stickyNavTop = $('.secondary-nav').offset().top;
+// $(document).on('turbolinks:load', function() { 
+//   var stickyNavTop = $('.secondary-nav').offset().top;
     
-  // our function that decides weather the navigation bar should have "fixed" css position or not.
-  var stickyNav = function(){
-    var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+//   // our function that decides weather the navigation bar should have "fixed" css position or not.
+//   var stickyNav = function(){
+//     var scrollTop = $(window).scrollTop(); // our current vertical position from the top
          
-    // if we've scrolled more than the navigation, change its position to fixed to stick to top,
-    // otherwise change it back to relative
-    if (scrollTop > stickyNavTop) { 
-        $('.secondary-nav').addClass('sticky');
-    } else {
-        $('.secondary-nav').removeClass('sticky'); 
-    }
-  };
+//     // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+//     // otherwise change it back to relative
+//     if (scrollTop > stickyNavTop) { 
+//         $('.secondary-nav').addClass('sticky');
+//     } else {
+//         $('.secondary-nav').removeClass('sticky'); 
+//     }
+//   };
 
-  stickyNav();
-  // and run it again every time you scroll
-  $(window).scroll(function() {
-    stickyNav();
-  });
-});
-
-$( document ).on('turbolinks:load', function() { 
-  $('.top-nav-carousel').owlCarousel({
-    loop:true,
-    responsiveClass:true,
-    responsive:{
-      0:{
-          items:1,
-          nav:false
-      },
-      600:{
-          items:3,
-          nav:false
-      },
-    }
-  })
-});
+//   stickyNav();
+//   // and run it again every time you scroll
+//   $(window).scroll(function() {
+//     stickyNav();
+//   });
+// });
 
